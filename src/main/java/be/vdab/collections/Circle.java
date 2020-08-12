32bd699076645485b2933a7daf68fafafb1c93af
Marc J <john.doe.gmail.com>
2020-08-12 14:57:21 +0200

    Comparator interface

---
+++ src/main/java/be/vdab/collections/Circle.java
@@ -0,0 +1,28 @@
+package be.vdab.collections;
+
+public class Circle extends GeometricObject{
+    private double radius;
+    private final double PI = 3.14;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return radius * radius * PI;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2*(radius * PI);
+    }
+}
