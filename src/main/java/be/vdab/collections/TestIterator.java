6e1a1be733bba555bce35c7a4a1fdde946e46932
Marc J <john.doe.gmail.com>
2020-08-12 11:15:20 +0200

    Iterator and forEach

---
+++ src/main/java/be/vdab/collections/TestIterator.java
@@ -0,0 +1,27 @@
+package be.vdab.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class TestIterator {
+    public static void main(String[] args) {
+        Collection<String> collection = new ArrayList<String>();
+        collection.add("New York");
+        collection.add("Atlanta");
+        collection.add("Dallas");
+        collection.add("Madison");
+
+        System.out.println("collection met iterator...");
+        Iterator<String> iterator = collection.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next().toUpperCase() + " ");
+        }
+
+        System.out.println("de clone zndr iterator mr met forEach...");
+        Collection<String> colClone = (Collection<String>) ((ArrayList<String>) collection).clone();
+
+        colClone.forEach(e -> System.out.println(e.toUpperCase() + " "));
+
+    }
+}
